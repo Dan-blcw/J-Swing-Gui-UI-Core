3e388eed55030d533f3699c3bed6bf61ddcb3e18
Dan-blcw <127305381+Dan-blcw@users.noreply.github.com>
2023-03-22 04:49:32 +0700

    Update Menu.java

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -35,8 +35,8 @@ public class Menu extends javax.swing.JPanel {
         g2.setColor(getBackground());
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         GradientPaint gp = new GradientPaint(0, 0, Color.decode("#a3c9e2"), 0, getHeight(), Color.decode("#f9957f"));
-        g2.setPaint(gp);
-        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 90, 90);
+        g2.setPaint(gp);        
+        g2.fillRoundRect(2, 4, getWidth()-5, getHeight()-5, 90, 90);
         g2.fillRect(getWidth() , 0, getWidth(), getHeight());
         g2.fillRect(0, getHeight() , getWidth(), getHeight());
         super.paintComponent(grphcs);
