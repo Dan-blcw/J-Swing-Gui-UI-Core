ca1cf0185bd5f2edb2a30642b7d5d131500606aa
Dan-blcw <127305381+Dan-blcw@users.noreply.github.com>
2023-03-21 09:27:30 +0700

    Create Menu.java

---
+++ src/view/Menu.java
@@ -0,0 +1,45 @@
+package view;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+public class Menu extends javax.swing.JPanel {
+
+    public Menu() {
+        initComponents();
+        setOpaque(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 188, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 178, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setColor(getBackground());
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint gp = new GradientPaint(0, 0, Color.decode("#a3c9e2"), 0, getHeight(), Color.decode("#f9957f"));
+        g2.setPaint(gp);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 90, 90);
+        g2.fillRect(getWidth() , 0, getWidth(), getHeight());
+        g2.fillRect(0, getHeight() , getWidth(), getHeight());
+        super.paintComponent(grphcs);
+    }
+
+}
