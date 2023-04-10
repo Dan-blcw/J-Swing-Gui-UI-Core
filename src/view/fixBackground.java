a16ab349dedae33fa055fff788f10c9a5f6ed563
Dan-blcw <127305381+Dan-blcw@users.noreply.github.com>
2023-04-11 01:35:48 +0700

    Update fixBackground.java

--- src/view/fixBackground.java
+++ src/view/fixBackground.java
@@ -34,9 +34,9 @@ public class fixBackground extends javax.swing.JPanel {
         Graphics2D g2 = (Graphics2D) grphcs;
         g2.setColor(getBackground());
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        GradientPaint gp = new GradientPaint(0, 0, Color.decode("#a3c9e2"), 0, getHeight(), Color.decode("#f9957f"));
-        g2.setPaint(gp);        
-        g2.fillRoundRect(2, 4, getWidth()-5, getHeight()-5, 90, 90);
+        GradientPaint gp = new GradientPaint(0, 0, Color.decode("#fff1eb"), 0, getHeight(), Color.decode("#517fa4"));
+        g2.setPaint(gp);
+        g2.fillRoundRect(4, 4, getWidth()-8, getHeight()-8, 90, 90);
         g2.fillRect(getWidth() , 0, getWidth(), getHeight());
         g2.fillRect(0, getHeight() , getWidth(), getHeight());
         super.paintComponent(grphcs);
